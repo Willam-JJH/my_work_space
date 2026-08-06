9fbe5d50b9c087895687b7b7ca3301cc1e96cb98
JJH_LP <william_0208@foxmail.com>
2026-08-07 01:20:07 +0800

    练题呢

--- practice/cpp/main.cpp
+++ practice/cpp/main.cpp
@@ -7,10 +7,10 @@ typedef string str;
 typedef pair<ll, ll> pll;
 #define lowbit(x) (x & -x)
 
-const int N = 1e2 + 10;
+const int N = 1e3 + 10;
 
 ll a[N];
-ll p[N];
+ll pre[N];
 
 void init()
 {
@@ -18,13 +18,26 @@ void init()
 
 void solve()
 {
-    ll n;
+    memset(a, 0, sizeof(a));
+    memset(pre, 0, sizeof(pre));
+    ll n, ans = 0;
     scanf("%lld", &n);
     for (int i = 1; i <= n; i++)
-        scanf("%lld", &a[i]);
-    partial_sum(a, a + n, p);
+    {
+        char ch;
+        scanf(" %c", &ch);
+        a[i] = ch - '0';
+    }
+    partial_sum(a + 1, a + n + 1, pre + 1);
     for (int i = 1; i <= n; i++)
-        printf("%lld", p[i]);
+    {
+        for (int j = i; j <= n; j++)
+        {
+            if (pre[j] - pre[i - 1] == j - i + 1)
+                ans++;
+        }
+    }
+    printf("%lld\n", ans);
 }
 
 int main()
